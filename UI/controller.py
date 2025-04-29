dbcab03220a7585e73781c34ae2dbfdd8375616e
Andrewtheprince <andry.mazzo@outlook.com>
2025-04-29 11:20:33 +0200

    first commit

--- UI/controller.py
+++ UI/controller.py
@@ -3,15 +3,15 @@ import flet as ft
 
 class Controller:
     def __init__(self, view, model):
-        # the view, with the graphical elements of the UI
         self._view = view
-        # the model, which implements the logic of the program and holds the data
         self._model = model
 
-    def handle_hello(self, e):
-        name = self._view.txt_name.value
-        if name is None or name == "":
-            self._view.create_alert("Inserire il nome")
+    def handleAnalizzaAeroporti(self, e):
+        distanzaMassima = self._view._txtIn.value
+        if distanzaMassima.isdigit():
+            distanzaMassima = int(distanzaMassima)
+        else:
+            self._view.create_alert("Devi inserire un valore numerico!")
+            self._view._txtIn.clean()
             return
-        self._view.txt_result.controls.append(ft.Text(f"Hello, {name}!"))
-        self._view.update_page()
+
--- UI/view.py
+++ UI/view.py
@@ -1,17 +1,17 @@
 import flet as ft
+from flet_core import MainAxisAlignment
 
 
 class View(ft.UserControl):
     def __init__(self, page: ft.Page):
         super().__init__()
-        # page stuff
+        self._btnAnalizzaAeroporti = None
+        self._txtIn = None
         self._page = page
         self._page.title = "Template application using MVC and DAO"
         self._page.horizontal_alignment = 'CENTER'
-        self._page.theme_mode = ft.ThemeMode.DARK
-        # controller (it is not initialized. Must be initialized in the main, after the controller is created)
+        self._page.theme_mode = ft.ThemeMode.LIGHT
         self._controller = None
-        # graphical elements
         self._title = None
         self.txt_name = None
         self.btn_hello = None
@@ -19,27 +19,13 @@ class View(ft.UserControl):
         self.txt_container = None
 
     def load_interface(self):
-        # title
-        self._title = ft.Text("Hello World", color="blue", size=24)
+        self._title = ft.Text("Flight Manager", color="blue", size=24)
         self._page.controls.append(self._title)
-
-        #ROW with some controls
-        # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
-            width=200,
-            hint_text="Insert a your name"
-        )
-
-        # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
-                      alignment=ft.MainAxisAlignment.CENTER)
-        self._page.controls.append(row1)
-
-        # List View where the reply is printed
+        self._txtIn = ft.TextField(label="Distanza Minima")
+        self._btnAnalizzaAeroporti = ft.ElevatedButton(text="Analizza Aeroporti", on_click=self._controller.handleAnalizzaAeroporti)
+        row1 = ft.Row([self._txtIn, self._btnAnalizzaAeroporti], alignment=MainAxisAlignment.CENTER, spacing=30)
         self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
-        self._page.controls.append(self.txt_result)
+        self._page.add(row1, self.txt_result)
         self._page.update()
 
     @property
